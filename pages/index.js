d34f841feea7fffbb8259cf71226a00c6ce3a824
GenaPlem <shainkill1337@gmail.com>
2024-11-18 11:36:18 +0000

    Add form to homepage

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,19 @@ function HomePage() {
   return (
     <div>
       <h1>The Home Page</h1>
+      <form>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <br />
+          <input type="email" id="email" />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <br />
+          <textarea name="feedback" id="feedback" rows="5"></textarea>
+        </div>
+        <button>Send Feedback</button>
+      </form>
     </div>
   );
 }
